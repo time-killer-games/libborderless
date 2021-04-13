0f9c70d796d39fb8e2f9f124e0262da7446a3e03
time-killer-games <samuelvenable@hotmail.com>
2021-04-13 19:48:26 -0400

    Add files via upload

--- build32.sh
+++ build32.sh
@@ -6,7 +6,7 @@ if [ $(uname) = "Darwin" ]; then
 elif [ $(uname) = "Linux" ]; then
   g++ borderless.cpp -o libborderless.so -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -m32
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ borderless.cpp -o libborderless.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -lX11 -m32
+  clang++ borderless.cpp -o libborderless.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -lX11 -m32 -fPIC
 else
   g++ borderless.cpp -o libborderless.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -m32
 fi
--- build64.sh
+++ build64.sh
@@ -6,7 +6,7 @@ if [ $(uname) = "Darwin" ]; then
 elif [ $(uname) = "Linux" ]; then
   g++ borderless.cpp -o libborderless.so -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -m64
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ borderless.cpp -o libborderless.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -lX11 -m64
+  clang++ borderless.cpp -o libborderless.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -lX11 -m64 -fPIC
 else
   g++ borderless.cpp -o libborderless.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -m64
 fi
