830cb194f0c8d3e06a6932370d62726dc304bc8c
time-killer-games <samuelvenable@hotmail.com>
2021-07-25 00:35:12 -0400

    Update build64.sh

--- build64.sh
+++ build64.sh
@@ -2,7 +2,7 @@
 cd "${0%/*}"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ borderless.mm -o libborderless.dylib -std=c++17 -shared -ObjC++ -framework Cocoa -m64
+  clang++ borderless.mm -o libborderless.dylib -std=c++17 -shared -ObjC++ -framework Cocoa -arch arm64 -arch x86_64
 elif [ $(uname) = "Linux" ]; then
   g++ borderless.cpp -o libborderless.so -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -m64
 elif [ $(uname) = "FreeBSD" ]; then
